65372491cb879111c68ead495a75a86e6e69f9a6
sien <sptw88@gmail.com>
2024-08-30 15:55:09 +0900

    model

--- nets/Model.py
+++ nets/Model.py
@@ -11,6 +11,7 @@ class MultiheadSelfAttention(torch.nn.Module):
     key pad mask shape must be B,512,512
     trg mask shape must be 1,512,512
     '''
+
     def __init__(self, config):
         super().__init__()
         self.attention_head_n = config.attention_head_n
@@ -37,17 +38,162 @@ class MultiheadSelfAttention(torch.nn.Module):
         energy = (Q @ K.transpose(-2, -1)) / math.sqrt(self.embd_dim)
 
         if mask is not None:
-            energy=energy.masked_fill(mask == 0, float('-inf'))
+            energy = energy.masked_fill(mask == 0, float('-inf'))
         softmax_energy = F.softmax(energy, -1)
         result = softmax_energy @ V
         result = result.transpose(1, 2).contiguous().view(B, S, E)
         return result
 
 
+class PosAndWordEmbedding(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.embd_layer = nn.Embedding(self.config.vocab_size, self.config.embd_dim)
+        self.pos_embd = nn.Embedding(self.config.block_size, self.config.embd_dim)
+        self.drop = nn.Dropout(0.1)
+
+    def forward(self, x):
+        b, t = x.size()
+        x = self.embd_layer(x)
+
+        device = x.device
+
+        pos_raw_ids = torch.arange(0, t, dtype=torch.long, device=device)
+
+        pos = self.pos_embd(pos_raw_ids)
+
+        x = self.drop(x + pos)
+        return x
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.embd_dim, 2 * config.embd_dim, bias=False)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(2 * config.embd_dim, config.embd_dim, bias=False)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.head = MLP(config)
+        self.drop = nn.Dropout(0.1)
+        self.att = MultiheadSelfAttention(config)
+        self.norm = nn.LayerNorm(config.embd_dim, bias=False)
+
+    def forward(self, x, attention_mask=None):
+        x = self.att(x, x, x, mask)
+        res = x
+        x = self.head(x)
+        x = res + x
+        x = self.norm(x)
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.ebd = PosAndWordEmbedding(config)
+        self.block_list = nn.ModuleList([TransformerBlock(config) for _ in range(config.encoder_layer_n)])
+
+    def forward(self, x, attention_mask=None):
+        x = self.ebd(x)
+        for block in self.block_list:
+            x = block(x, attention_mask)
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.att = MultiheadSelfAttention(config)
+        self.transformerblock = nn.ModuleDict(dict(
+            mhe=MultiheadSelfAttention(config),
+            mlp=MLP(config),
+            ln1=nn.LayerNorm(config.embd_dim, eps=1e-6),
+            ln2=nn.LayerNorm(config.embd_dim, eps=1e-6),
+            do=nn.Dropout(0.1),
+        ))
+
+    def forward(self, encoder_out, target, attn_mask, trg_mask):
+        residual = target
+
+        decoder_attention = self.att(target, target, target, trg_mask)
+
+        decoder_attention = self.transformerblock.do(decoder_attention + residual)
+        decoder_attention = self.transformerblock.ln1(decoder_attention)
+
+        mha_output = self.transformerblock.mhe(decoder_attention, encoder_out, encoder_out, attn_mask)
+
+        mha_output = self.transformerblock.do(mha_output + decoder_attention)
+        mha_output = self.transformerblock.ln2(mha_output)
+
+        res = mha_output
+        decoder_attention_output = self.transformerblock.mlp(mha_output)
+        decoder_attention_output = self.transformerblock.do(decoder_attention_output + res)
+        decoder_attention_output = self.transformerblock.ln2(decoder_attention_output)
+
+        return decoder_attention_output
+
+
+class Decoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.posembd = PosAndWordEmbedding(config)
+        self.blocks = nn.ModuleList([DecoderBlock(config) for _ in range(config.decoder_layer_n)])
+        self.drop = nn.Dropout(0.1)
+
+    def forward(self, target, encoder_out, attn_mask, trg_mask):
+        target = self.drop(self.posembd(target))
+        for block in self.blocks:
+            target = block(encoder_out, target, attn_mask, trg_mask)
+        return target
+
+
+class TS(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.encoder = Encoder(config)
+
+        self.decoder = Decoder(config)
+
+        self.fc_out = nn.Linear(config.embd_dim, config.vocab_size)
+
+    def make_trg_mask(self, seq):
+        S = seq.shape[1]
+        trg_mask = torch.tril(torch.ones((S, S), device=seq.device)).bool().unsqueeze(0)
+        return trg_mask
+
+    def forward(self, source, target, attention_mask=None):
+        trg_mask = self.make_trg_mask(target)
+        enc_out = self.encoder(source, attention_mask)
+        outputs = self.decoder(target, enc_out, attention_mask, trg_mask)
+        output = self.fc_out(outputs)
+
+        return output
+
+
 if __name__ == "__main__":
     config = TSConfig()
-    dummy = torch.randn(size=(8, 512, 128))
+    dummy = torch.randint(10, 100, size=(8, 512))
     mask = torch.ones(size=(8, 512, 512))
     trg_mask = torch.tril(torch.ones((512, 512))).bool().unsqueeze(0)
-    att = MultiheadSelfAttention(config)
-    a = att(dummy, dummy, dummy, mask)
+    M = TS(config)
+    a = M(dummy,dummy, mask)
